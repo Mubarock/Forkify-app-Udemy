a331f728c214838a9d03d23263dd4abb480488ef
Mubarock <mubbybiodun25@gmail.com>
2023-02-11 11:06:55 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e9884afd-1ea5-46aa-b5fa-ebb3dc02e024';
+export const MODEL_CLOSE_SEC = 2.5;
---
+++ src/js/views/resultView.js
@@ -0,0 +1,15 @@
+import view from './view.js';
+import previewView from './previewView.js';
+import icons from 'url:../../img/icons.svg';
+
+class ResultView extends view {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipe found for your query, please try another one!';
+  _message = '';
+
+  _generateMarkup() {
+    return this._data.map(result => previewView.render(result, false)).join('');
+  }
+}
+
+export default new ResultView();
